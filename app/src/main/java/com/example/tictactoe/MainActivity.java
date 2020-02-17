6994d05a0e805f302181acad4e7090ce3dc2b80b
Clibom <valentinmetz.z@gmail.com>
2020-02-17 11:19:20 +0100

    [NEW] Add first elements of TicTacToe layout

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -11,4 +11,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //TODO Match elements of XML Layout here see https://developer.android.com/reference/android/view/View.html#findViewById(int)
+
+
+        //TODO Create separate listeners for different clickable elements (image, play button, etc...)
+    }
+
+    protected void buildGame(){
+        //TODO OPTINAL Build the parameter of the party (player names, image choice, etc...)
+    }
 }
